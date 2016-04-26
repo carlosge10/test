dc357ca7f6be8ee952c9e39c030066e7412c7f64
Kireck211 <erickdesantiago11@gmail.com>
2016-04-26 16:56:34 -0500

    Classes

---
+++ Drop/Drop/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drop
+{
+    class Class2
+    {
+        public string nombre2;
+        public int edad2;
+    }
+}
